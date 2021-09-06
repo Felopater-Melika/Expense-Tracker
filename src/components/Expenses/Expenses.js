a997a479ff3ff6445e12dc5498b3d69403f301df
Felopater <68111601+Felopater2004@users.noreply.github.com>
2021-09-05 21:16:06 -0500

    FInished expense tracker

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
-const Expenses = (props) => {
+const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState('2021');
 
-  const filterChangeHandler = (selectedYear) => {
+  const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter((expense) => {
+  const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
@@ -22,14 +24,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = (props) => {
+const ExpenseForm = props => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
@@ -13,7 +13,7 @@ const ExpenseForm = (props) => {
   //   enteredDate: '',
   // });
 
-  const titleChangeHandler = (e) => {
+  const titleChangeHandler = e => {
     setEnteredTitle(e.target.value);
     // setUserInput({
     //   ...userInput,
@@ -24,7 +24,7 @@ const ExpenseForm = (props) => {
     // });
   };
 
-  const amountChangeHandler = (e) => {
+  const amountChangeHandler = e => {
     setEnteredAmount(e.target.value);
     // setUserInput({
     //   ...userInput,
@@ -35,7 +35,7 @@ const ExpenseForm = (props) => {
     // });
   };
 
-  const dateChangeHandler = (e) => {
+  const dateChangeHandler = e => {
     setEnteredDate(e.target.value);
     // setUserInput({
     //   ...userInput,
@@ -46,12 +46,12 @@ const ExpenseForm = (props) => {
     // });
   };
 
-  const submitHandler = (e) => {
+  const submitHandler = e => {
     e.preventDefault();
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -94,6 +94,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
